0f31bea9514eccfb4b28cc2f5cb3e8f12170562e
Artemka0101 <64738836+Artemka0101@users.noreply.github.com>
2022-12-17 13:56:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ while(cap.isOpened()):
     emo = emo[0]
     emo = emo['emotions']
     #print(emo, end=' ')
-    score = emo['happy']+emo['surprise']-emo['angry']-emo['disgust']-emo['fear']-emo['sad'] #расчёт, насколько понравилось пользователю видео
+    score = emo['happy']+emo['surprise']-emo['neutral']-emo['angry']*3-emo['sad']*2 #расчёт, насколько понравилось пользователю видео
     #print(score)
-    if score > 0.5 and liked == False: #проверка, понравилось ли пользователю видео
+    if score > 0.3 and liked==False: #проверка, понравилось ли пользователю видео
         keyboard.send('l') #ставим лайк
         liked = True
 
@@ -47,17 +47,17 @@ while(cap.isOpened()):
             positions.pop(0) #удаляем из списка координаты, полученные слишком давно
         for i in range(0, len(positions)):
             if abs(positions[i][0][0] - positions[len(positions)-1][0][0]) < 100: #проверяем, что по оси x движение было коротким
-                if positions[i][0][0] - positions[len(positions)-1][0][1] > 30 and positions[i][0][0] - positions[len(positions)-1][0][1] < 300 and "Right" in str(results.multi_handedness[0]): #проверяем движение вверх по оси y
+                if positions[i][0][0] - positions[len(positions)-1][0][1] > 30 and positions[i][0][0] - positions[len(positions)-1][0][1] < 300 and "Right" in str(results.multi_handedness[0]): #проверяем движение вверх по оси y и что его делает правая рука
                     keyboard.send("down") #идём к следующему видео
                     liked = False
                     break
-                if positions[i][0][0] - positions[len(positions)-1][0][1] < -30 and positions[i][0][0] - positions[len(positions)-1][0][1] > -300 and "Left" in str(results.multi_handedness[0]): #проверяем движение вниз по оси y
+                if positions[i][0][0] - positions[len(positions)-1][0][1] < -30 and positions[i][0][0] - positions[len(positions)-1][0][1] > -300 and "Left" in str(results.multi_handedness[0]): #проверяем движение вниз по оси y и что его делает левая рука
                     keyboard.send("up") #идём к предыдущему видео
                     break
-                
+
         #cv2.circle(flippedRGB, (x_tip, y_tip), 10, (255, 0, 0), -1)
-    #res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-    #cv2.imshow("Hands", res_image)
-    count += 1 
+    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
+    cv2.imshow("TT-helper", res_image)
+    count += 1
 
 handsDetector.close()
